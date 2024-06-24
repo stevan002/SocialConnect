bd01676d44fae6ce70508bbc6d0a8033ea390ee8
stevan002 <stevanstankovic603@gmail.com>
2024-06-24 17:45:07 +0200

    create project

---
+++ SocialConnect/src/test/java/com/example/SocialConnect/SocialConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SocialConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
